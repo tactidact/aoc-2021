f8e0c504ef2af65a9f4b17c0ba6ad0da4215cd3c
tactidact <jetdra@gmail.com>
2021-12-01 10:32:09 -0500

    cleanup and breaking up into functions

--- 1/1.py
+++ 1/1.py
@@ -1,9 +1,10 @@
-def day_1():
+def file_read():
     with open('input.txt') as f:
         depth_list = f.readlines()
+    return depth_list
 
-    depth_list = list(map(lambda s: s.strip(), depth_list))
 
+def part_1(depth_list):
     increased = 0
     decreased = 0
 
@@ -14,7 +15,10 @@ def day_1():
             decreased += 1
 
     print('Part 1', increased, decreased)
+    # Part 1 1759 240
 
+
+def part_2(depth_list):
     sums = []
     for i in range(len(depth_list)-1):
         if i < len(depth_list)-2:
@@ -31,6 +35,17 @@ def day_1():
             decreased += 1
 
     print('Part 2', increased, decreased)
+    # Part 2 1805 176
+
+
+def day_1():
+    depth_list = file_read()
+
+    depth_list = [int(line.strip()) for line in depth_list]
+
+    part_1(depth_list)
+
+    part_2(depth_list)
 
 
 if __name__ == "__main__":
